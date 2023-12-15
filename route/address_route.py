cfe09410d10c5a6ea5775ef3bd5f9fd7a43a88b4
qm0921 <yang.qingming@tudb.ai>
2023-12-15 14:28:26 +0800

    update

--- route/address_route.py
+++ route/address_route.py
@@ -1,13 +1,28 @@
+import json
+
 from fastapi import APIRouter, Request
+
+from framework.result_enc import suc_enc
 
 router = APIRouter()
 
 
 @router.get("/address/config")
 async def get_config():
-    return
+    networks_file = "static/networks.json"
+    categories_file = "static/categories.json"
+
+    return suc_enc({
+        "networks": get_json_data(networks_file),
+        "categories": get_json_data(categories_file)
+    })
 
 
 @router.post("/address/report")
 async def report_address():
     return
+
+
+def get_json_data(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        return json.load(f)
