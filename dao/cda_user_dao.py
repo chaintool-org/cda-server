e5dfb82936777ea02f8b6881035bccf0922ed222
qm0921 <yang.qingming@tudb.ai>
2023-12-19 15:30:51 +0800

    update

--- dao/cda_user_dao.py
+++ dao/cda_user_dao.py
@@ -3,3 +3,7 @@ from dao.models import CdaUser
 
 async def get_cda_user_by_connect_info(connect_type: str, connect_id: str) -> CdaUser:
     return await CdaUser.single('connect_type = %s and connect_id = %s', connect_type, connect_id)
+
+
+async def get_cda_user_by_id(connect_type: str, id: int, status: int = 0) -> CdaUser:
+    return await CdaUser.single('connect_type = %s and id = %s and status = %s', connect_type, id, status)
