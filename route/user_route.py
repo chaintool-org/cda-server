229d9a8c0cf6e2f9db08f83928043b1e64b5d1b3
qm0921 <yang.qingming@tudb.ai>
2024-09-25 15:50:11 +0800

    修改commit

--- route/user_route.py
+++ route/user_route.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request
 
 from dao.cda_organization_dao import get_all_valid_organizations
 from dao.models import CdaUser
+from framework.error_message import TG_USER_NOT_REGISTERED, TG_ID_NOT_FOUND, ORG_NOT_FOUND, NICKNAME_NOT_FOUND
 from framework.exceptions import BusinessException
 from framework import errorcode
 from dao import cda_user_dao
@@ -14,7 +15,7 @@ router = APIRouter()
 @router.get("/user/tg/get_id")
 async def get_cda_by_tg_id(tgId: str = None):
     if tgId is None or tgId.strip() is False:
-        raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL, 'telegram id is not present')
+        raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL, TG_ID_NOT_FOUND)
 
     # 根据tgId查询用户
     cda_user: CdaUser = await cda_user_dao.get_cda_user_by_connect_info(constants.CONNECT_TYPE_TELEGRAM, tgId)
@@ -31,15 +32,15 @@ async def get_cda_by_tg_id(tgId: str = None):
 @router.post("/user/tg/connect")
 async def connect_tg(tgId: str = None, org: str = None, nickname: str = None):
     if tgId is None or not tgId.strip():
-        raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL, 'telegram id is not present')
+        raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL, TG_ID_NOT_FOUND)
     if org is None:
-        raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL, 'org not present')
+        raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL, ORG_NOT_FOUND)
     if nickname is None or not nickname.strip():
-        raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL, 'nickname not present')
+        raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL, NICKNAME_NOT_FOUND)
 
     ol = await get_all_valid_organizations()
     if org not in ol:
-        raise BusinessException(errorcode.ORGANIZATION_NOT_EXIST, "organization not exist")
+        raise BusinessException(errorcode.ORGANIZATION_NOT_EXIST, ORG_NOT_FOUND)
 
     # 判断tgid是否已存在
     cda_user: CdaUser = await cda_user_dao.get_cda_user_by_connect_info(constants.CONNECT_TYPE_TELEGRAM, tgId)
@@ -49,4 +50,4 @@ async def connect_tg(tgId: str = None, org: str = None, nickname: str = None):
             'cdaId': cda_user.id
         })
     raise BusinessException(errorcode.REQUEST_PARAM_ILLEGAL,
-                            "To use the CDA TG bot, you must first be a member of the CDA TG channel. Please contact your administrator to be added to the channel.")
+                            TG_USER_NOT_REGISTERED)
--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,18 @@
+import json
+import os
+
 CONNECT_TYPE_TELEGRAM = 'TELEGRAM'
+
+
+# 发消息的testMode
+test_mode = json.loads(os.getenv('SEND_MESSAGE_LIST', '["dev", "test", "prod"]'))
+# 发送消息的token
+send_message_token = json.loads(os.getenv('SEND_MESSAGE_TOKEN',
+                                          '{"dev": {"token": "6767843762:AAGSVGuWWW7bAqRLProaQxPbN1MzLpmZS1g",'
+                                          ' "chat_id": "-4539437068","tg_name":"@qimashejian2Bot"},'
+                                          '"test": {"token": "6719660314:AAHnyhfgS6An6Ff2RcjT679xPWLhgSXfseo",'
+                                          ' "chat_id": "-1002111465087"},'
+                                          '"prod": {"token": "6566268578:AAGc4sGBLHlIpCBBtUBCJDVu4fzP5IdhUeg", '
+                                          '"chat_id": "-1001280903433","tg_name":"@CDAReporterBot"} }'))
+
 
